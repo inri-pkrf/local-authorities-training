bab7ded027c409e1641f6d1218e86428d3a67b72
naama-yanovsky <naama.yanovsky@gmail.com>
2024-05-08 13:54:12 +0300

    fixing things

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Home from './Home'; // Import the Home component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Home /> {/* Include the Home component */}
+      {/* Main content of your app */}
     </div>
   );
 }
