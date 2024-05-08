d2d2f8606cc54ba505031ca12352edc7cd165cf8
naama-yanovsky <naama.yanovsky@gmail.com>
2024-05-08 13:56:39 +0300

    fb

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home'; // Import the Home component
+import Diagram from './Diagram';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home /> {/* Include the Home component */}
+      <Diagram /> {/* Include the Home component */}
       {/* Main content of your app */}
     </div>
   );
