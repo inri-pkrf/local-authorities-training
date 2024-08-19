aaba00b70edec341fb801b7d2b769673cac40d20
Alma Yovell <alma.yovell@gmail.com>
2024-08-19 16:43:03 +0300

    תיקוני עיצוב- משוב

--- my-app/src/Menu.js
+++ my-app/src/Menu.js
@@ -16,11 +16,17 @@ function Menu({ onClose, onNavigate }) {
       <img src={process.env.PUBLIC_URL + '/closeBtn.png'} alt="Close" onClick={onClose} className="closeBtn" />
       <ul className="menu-list">
         <li onClick={() => handleItemClick('home')}>עמוד הבית</li>
+        <div className='lineMenu'></div>
         <li onClick={() => handleItemClick('diagram')}>מבנה הרשות המקומית בחירום</li>
+        <div className='lineMenu'></div>
         <li onClick={() => handleItemClick('gallery')}> הערכת מצב ברשות המקומית בשעת חירום</li>
+        <div className='lineMenu'></div>
         <li onClick={() => handleItemClick('relations')}>ממשקי עבודה בין מכלולים</li>
+        <div className='lineMenu'></div>
         <li onClick={() => handleItemClick('target')}> יעדים לאומיים </li>
+        <div className='lineMenu'></div>
         <li id='last-item-m' onClick={() => handleItemClick('ogen')}> תרחיש ייחוס מישקי</li>
+        <div className='lineMenu' id='lineMenuLast'></div>
       </ul>
 
       <div className='mashov-menu'>
